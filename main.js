24307d098c077a87b325f7b11fbdf8736944a6d2
caeser lettuce <84699501+caeserlettuce@users.noreply.github.com>
2024-02-23 12:11:08 -0800

    hi

--- main.js
+++ main.js
@@ -5,7 +5,30 @@ var stuff = {
 }
 
 
+
+for (i in stuff["backgrounds"]) {
+  var node = document.querySelector("img.bg-choose").cloneNode(true);
+  node.setAttribute("src", `${stuff["backgrounds"][i]}`);
+  node.setAttribute("onclick", `change_background(${i})`);
+  node.classList.remove("template");
+  document.querySelector(".background-choose").appendChild(node);
+}
+
 function on_text_change() {
-  var text = document.querySelector("input.inputtext").value;
+  document.querySelector(".funny h2").innerHTML = document.querySelector("input.inputtext").value;
   
+}
+
+function change_background(id) {
+  document.querySelector(".funny img").setAttribute("src", `${stuff["backgrounds"][id]}`);
+}
+
+
+function download_image()
+{
+    var c = document.querySelector('.funny');
+    var t = c.getContext('2d');
+    window.location.href = image;
+
+    window.open('', document.querySelector('.funny').toDataURL());
 }
